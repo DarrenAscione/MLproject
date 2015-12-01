923f2134cdcf8e32dbf2baaf6f69e0a5b0c8372f
DarrenAscione <darren_ng@mymail.sutd.edu.sg>
2015-12-02 01:27:16 +0800

    conflict

--- emission_MLE.py
+++ emission_MLE.py
@@ -1,5 +1,7 @@
+#Structure of the count_word = {tag: {word: count, word: count}, tag:{word, count}}
 
-
+# training.py outputs 2 file: bjo file and count file
+# testing.py output annotation file and bjo file
 def count_word(filename):
 	dict = {}
 	with open(filename) as file:
@@ -14,9 +16,16 @@ def count_word(filename):
 					dict[words[1]][words[0]] = 0
 				dict[words[1]][words[0]] += 1
 	return dict
-
+#change later
 count_states = lambda dict_word: sum(dict_word.values())
 
+def count_all_states(word_count):
+	states = {}
+	for key in word_count.keys():
+		states[key] = count_states(word_count[key])
+	return states
+
+# Computes the bjos
 def gen_bjo(filename, mode="training"):
 	word_count = count_word(filename)
 	for key in word_count.keys():
@@ -24,7 +33,7 @@ def gen_bjo(filename, mode="training"):
 			if mode == "training":
 				tag_count = count_states(word_count[key])
 			else:
-				tag_count = count_states(word_count[key]+1)
+				tag_count = count_states(word_count[key])+1
 			word_count[key][words] = word_count[key][words]*1.0/ tag_count
 	return word_count
 
@@ -51,20 +60,51 @@ def testing_splitter(filename):
 				one_dataset = []
 	return total_dataset
 
-def emission_prob_Tagger(filetest, filetrain, file_write):
+# def emission_prob_Tagger(filetest, filetrain, file_write):
+# 	dataset = testing_splitter(filetest)
+# 	word_count = count_word(filetrain)
+# 	states_count = count_states(filetrain)
+# 	# Generate table of bjos from training data
+# 	bjos = gen_bjo(filetrain, mode = "training")
+# 	# Generate file to save generate bjos of testing data
+# 	with open(file_write, "w") as file:
+# 		for sets in xrange(len(dataset)):
+# 			for words in dataset[sets]:
+# 				temp = 0
+# 				temp_arg = ""
+# 				for tags in bjos.keys():
+# 					# if word found in training bjo table
+# 					if words in bjos[tags].keys():
+# 						if bjos[tags][words] >= temp: #replace with highest prob
+# 							temp = bjos[tags][words]
+# 							temp_arg = tags
+
+# 				if temp == 0:
+# 					for tag in states_count.keys():
+# 						if states_count[tag] == min(states_count.values()):
+# 							temp_arg = tag
+# 				file.write("%s %s\n"%(words,temp_arg))
+# 			file.write("\n")
+# 	return file_write
+
+def emission_prob(filetest, filetrain, file_write):
 	dataset = testing_splitter(filetest)
-	states_count = count_states(filetrain)
-	bjos = gen_bjo(filetrain, mode = "training")
+	word_count = count_word(filetrain)
+	states_count = count_all_states(word_count)
+	bjos = gen_bjo(filetrain, mode="testing")
+	# Generate file to save generate bjos of testing data
 	with open(file_write, "w") as file:
 		for sets in xrange(len(dataset)):
 			for words in dataset[sets]:
 				temp = 0
 				temp_arg = ""
 				for tags in bjos.keys():
+					# if word found in training bjo table
 					if words in bjos[tags].keys():
-						if bjos[tags][words] >= temp:
+						if bjos[tags][words] >= temp: #replace with highest prob
 							temp = bjos[tags][words]
 							temp_arg = tags
+				# word does not appear
 				if temp == 0:
 					for tag in states_count.keys():
 						if states_count[tag] == min(states_count.values()):
@@ -72,12 +112,6 @@ def emission_prob_Tagger(filetest, filetrain, file_write):
 				file.write("%s %s\n"%(words,temp_arg))
 			file.write("\n")
 	return file_write
-
-# def pos_tagger(filetest, filetrain):
-# 	tagged_list = []
-# 	dataset = testing_splitter(filetest)
-# 	bjos = gen_bjo(filetrain, mode = "training")
-
 
 def accuracy(original_file, predicted_file):
 	count = 0
@@ -101,9 +135,11 @@ def tag_extractor(filename):
 test_file = "POS/dev.in"
 correct_file = "POS/dev.out"
 train_file = "NPC/train"
-save_file = "POS/copy1.txt"
-# predicted = emission_prob_Tagger(test_file, train_file, save_file)
+save_file = "POS/copy2.txt"
+predicted = emission_prob(test_file, correct_file, save_file)
 # print accuracy(correct_file, predicted)
 
-output_to_file(gen_bjo(train_file), "emissionReadable")
+# output_to_file(gen_bjo(train_file), "emissionReadable")
+
+
 
