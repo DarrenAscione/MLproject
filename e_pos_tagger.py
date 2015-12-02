930f6cfe87c3092659e361dcbb69587e68d7645a
DarrenAscione <darren_ng@mymail.sutd.edu.sg>
2015-12-02 13:55:35 +0800

    added pos tagger and accuracy fn

--- e_pos_tagger.py
+++ e_pos_tagger.py
@@ -1,8 +1,6 @@
-# def pos_tagger(filelist, testing_data, testing_tagger):
-# 	dict = {}
+# returns a file containing the dev.in with its predicted labels
 
 from testing import testing_splitter
-	
 
 def checker(testing_tagger, word):	
 	with open(testing_tagger, "r") as file:
@@ -12,7 +10,7 @@ def checker(testing_tagger, word):
 			if word == words[0]:
 				return words[1]
 
-def tagger(filetest, testing_tagger, file_write):
+def tag_all(filetest, testing_tagger, file_write):
 	dataset = testing_splitter(filetest, mode="ALL")
 	with open(file_write, "w") as file:
 		for sets in xrange(len(dataset)):
@@ -22,7 +20,7 @@ def tagger(filetest, testing_tagger, file_write):
 			file.write("\n")
 
 
-filetest = "POS/dev.in"
-testing_tagger = "POS/emission_testing_tags.txt"
-tagger(filetest, testing_tagger, "POS/dev.Predict")
+filetest = "NPC/dev.in"
+testing_tagger = "NPC/emission_testing_tags.txt"
+tag_all(filetest, testing_tagger, "NPC/dev.Predict")
 
